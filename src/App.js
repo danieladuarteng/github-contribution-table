348b81ddee90cf7d6de025979c469bd4f7185831
Daniela Duarte <falecom@danieladuarte.com.br>
2020-08-06 21:18:25 -0300

    feat: create table and add data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import { data } from './data'
 import './App.css';
 
 function App() {
+  const orderDate = data
+    .sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 
+  'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  const dateMonths = orderDate.map(({ date }) => {
+    return months[new Date(date).getMonth()]
+  })
+  
+  const removeMonthsDuplicates = dateMonths
+    .filter((item, index) => dateMonths.indexOf(item) === index);
+
+  const getCell = (day) => orderDate.filter(({date})=> {
+    const dateTransform = new Date(date);
+    const result = removeMonthsDuplicates
+      .includes(months[dateTransform.getMonth()])
+      && days[dateTransform.getDay()] === day
+    return result
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          {removeMonthsDuplicates.map(month => (
+            <th>{month}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {days.map(day => 
+          <tr>
+          <th>{day}</th>
+          {getCell(day).map(({date}) => (
+            <td>{date}</td>
+          ))}
+          </tr>
+        )}
+      </tbody>
+    </table>
   );
 }
 
