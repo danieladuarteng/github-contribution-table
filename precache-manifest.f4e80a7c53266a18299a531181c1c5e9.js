138606b5fe7feb0ab0256966370ebd101b16ad31
Daniela Duarte <falecom@danieladuarte.com.br>
2020-08-12 12:51:24 -0300

    Updates

---
+++ precache-manifest.f4e80a7c53266a18299a531181c1c5e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7032c54cb82a7f52cbdde410bc4f25d5",
+    "url": "/github-contribution-table/index.html"
+  },
+  {
+    "revision": "c712976f1e300de4999b",
+    "url": "/github-contribution-table/static/js/2.92bf625b.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/github-contribution-table/static/js/2.92bf625b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8f061f0602ba4f3403",
+    "url": "/github-contribution-table/static/js/main.33dd82f2.chunk.js"
+  },
+  {
+    "revision": "ecc29656ee549aeeb119",
+    "url": "/github-contribution-table/static/js/runtime-main.c234955e.js"
+  }
+]);
